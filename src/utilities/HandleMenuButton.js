6c54127a98d5bbab5b81f5ec844ea4c77d9faed3
TienPhamDev <manhtienbn18891@gmail.com>
2025-07-11 02:00:51 -0400

    code responsive screen tablet for home and headphone screen , add menu button

--- src/utilities/HandleMenuButton.js
+++ src/utilities/HandleMenuButton.js
@@ -2,12 +2,51 @@ const HandleMenuButton = () => {
   const menuButton = document.getElementById("menu-button");
   const svgClose = document.getElementById("menu-close");
   const svgOpen = document.getElementById("menu-open");
-  const menu = document.getElementById("menu");
+  const menuTablet = document.getElementById("menu-tablet");
+  const overlay = document.querySelector(".overlay-menu");
 
+  const funcHideMenu = () => {
+    svgClose.classList.toggle("hidden");
+    svgOpen.classList.toggle("hidden");
+    menuTablet.classList.toggle("hidden");
+    menuTablet.style.transform = "translateY(0)";
+    menuTablet.style.transition = "transform 0.3s ease";
+    overlay.style.display = "none";
+    document.body.style.overflowY = "auto";
+  };
   menuButton.addEventListener("click", () => {
     svgClose.classList.toggle("hidden");
     svgOpen.classList.toggle("hidden");
-    menu.classList.toggle("hidden");
+    menuTablet.classList.toggle("hidden");
+    if (menuTablet.classList.contains("hidden")) {
+      menuTablet.style.transform = "translateY(130%)";
+      menuTablet.style.transition = "transform 0.3s ease";
+      overlay.style.display = "block";
+      document.body.style.overflowY = "hidden";
+    } else {
+      menuTablet.style.transform = "translateY(0)";
+      menuTablet.style.transition = "transform 0.3s ease";
+      overlay.style.display = "none";
+      document.body.style.overflowY = "auto";
+    }
+
+    // menuTablet.style.transform = "translateY(130%)";
+    // menuTablet.style.transition = "transform 0.3s ease";
+    // hero.style.filter = "blur(3px)";
+    // document.body.style.overflowY = "hidden";
+  });
+  // Close the menu when clicking outside of it
+  document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("overlay-menu")) {
+      funcHideMenu();
+    }
+  });
+  // Close the menu when clicking on cart button
+  const cartButton = document.getElementById("cart-button");
+  cartButton.addEventListener("click", () => {
+    if (menuTablet.classList.contains("hidden")) {
+      funcHideMenu();
+    }
   });
 };
 
